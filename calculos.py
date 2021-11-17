aa26111f932e92b1e5a2b829a136b078c3816ab8
William S Teixeira <smooth.poa@gmail.com>
2021-11-17 16:29:43 -0300

    Adicionando soma

---
+++ calculos.py
@@ -0,0 +1,7 @@
+def soma(num1, num2):
+    return num1 + num2
+
+valor1 = int(input('Digite o primeiro valor: '))
+valor2 = int(input('Digite o segundo valor: '))
+
+print(f'Resultado = {soma(valor1, valor2)}')
