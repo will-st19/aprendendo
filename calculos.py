76d138c4fe5e4ae86a8f632f443de9224481b236
William S Teixeira <smooth.poa@gmail.com>
2021-11-17 17:33:34 -0300

    Adicionando Multiplicação

--- calculos.py
+++ calculos.py
@@ -6,9 +6,14 @@ def subtrair(num1, num2):
     return num1 - num2
 
 
+def multiplicar(num1, num2):
+    return num1 * num2
+
+
 valor1 = int(input('Digite o primeiro valor: '))
 valor2 = int(input('Digite o segundo valor: '))
 
 
 print(f'Resultado da soma = {somar(valor1, valor2)}')
 print(f'Resultado da subtração = {subtrair(valor1, valor2)}')
+print(f'Resultado da multiplicação = {multiplicar(valor1, valor2)}')
