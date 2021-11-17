7a8db031acb0ed26d32b9c563758aa8409eb8da5
William S Teixeira <smooth.poa@gmail.com>
2021-11-17 17:20:43 -0300

    Adicionando Subtração

--- calculos.py
+++ calculos.py
@@ -1,7 +1,14 @@
-def soma(num1, num2):
+def somar(num1, num2):
     return num1 + num2
+
+
+def subtrair(num1, num2):
+    return num1 - num2
+
 
 valor1 = int(input('Digite o primeiro valor: '))
 valor2 = int(input('Digite o segundo valor: '))
 
-print(f'Resultado = {soma(valor1, valor2)}')
+
+print(f'Resultado da soma = {somar(valor1, valor2)}')
+print(f'Resultado da subtração = {subtrair(valor1, valor2)}')
